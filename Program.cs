172784da32c46b680b1072066627b409180adeeb
Вадим <vadimmm.m@ya.ru>
2020-09-20 16:14:58 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace Pr2S2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter writer = File.CreateText("C:\\All\\newfile.txt");
+            writer.WriteLine("Learning cs is fun)0)");
+            writer.WriteLine("I'm done");
+            writer.Close();
+        }
+    }
+}
